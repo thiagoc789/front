b81e4c1336008f02dcfca8674bf2c5eb68675ba9
Santiago Castaño <santiago.cruz@correounivalle.edu.co>
2021-09-25 03:09:41 -0500

    Front con empezar encuesta y sin nada en la pagina principal, falta llenar y hacer mas bonita la pagina principal

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import AddTutorial from "./components/add-tutorial.component";
-import Tutorial from "./components/tutorial.component";
-import TutorialsList from "./components/tutorials-list.component";
+
 
 class App extends Component {
   render() {
@@ -13,17 +12,14 @@ class App extends Component {
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
           <a href="/tutorials" className="navbar-brand">
-            bezKoder
+            Eventos-Choclo
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
             </li>
             <li className="nav-item">
               <Link to={"/add"} className="nav-link">
-                Add
+                Empezar Encuesta
               </Link>
             </li>
           </div>
@@ -31,9 +27,9 @@ class App extends Component {
 
         <div className="container mt-3">
           <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            
             <Route exact path="/add" component={AddTutorial} />
-            <Route path="/tutorials/:id" component={Tutorial} />
+            
           </Switch>
         </div>
       </div>
