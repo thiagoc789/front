dd7aeff6364141a471cd83506f8de754d0efbe4e
Santiago Castaño <santiago.cruz@correounivalle.edu.co>
2021-09-25 17:39:55 -0500

    Envia Cedula, Nombre, Edad, Sexo

--- src/App.js
+++ src/App.js
@@ -1,37 +1,45 @@
 import React, { Component } from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route} from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+
 
 import AddTutorial from "./components/add-tutorial.component";
 
 
 class App extends Component {
+  
   render() {
-    return (
-      <div>
+    
+    return ( 
+              
+      <div>  
+        
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/tutorials" className="navbar-brand">
+          <a href="/tutorials" className="navbar-brand bg-dark">
             Eventos-Choclo
           </a>
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Empezar Encuesta
-              </Link>
-            </li>
+          <div className="navbar-nav mr-auto bg-dark">
           </div>
-        </nav>
+          
+        </nav> 
+        <h3 class="text-hide">Eventos Choclo</h3>  
+        <h6 class="text-hide">¡Bienvenidos!
+        
 
-        <div className="container mt-3">
-          <Switch>
-            
-            <Route exact path="/add" component={AddTutorial} />
-            
+Este formulario tiene como propósito conocer las diferentes actividades que realiza la población caleña y hacía qué tipo de eventos se decantan con mayor facilidad.
+
+¡No te preocupes!
+Tus datos no serán compartidos con terceros, el fin de la encuesta es netamente académico😄.</h6>     
+        <a href="/add" >
+      <button type="button" class="btn btn-primary btn-lg">Empezar Encuesta</button>
+      </a>  
+      
+                   
+          <Switch>          
+            <Route exact path="/add" component={AddTutorial} />        
           </Switch>
-        </div>
+        
       </div>
     );
   }
--- src/components/add-tutorial.component.js
+++ src/components/add-tutorial.component.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import TutorialDataService from "../services/tutorial.service";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 
 export default class AddTutorial extends Component {
   constructor(props) {
@@ -7,13 +9,15 @@ export default class AddTutorial extends Component {
     this.onChangeCedula = this.onChangeCedula.bind(this);
     this.onChangeNombre = this.onChangeNombre.bind(this);
     this.onChangeEdad = this.onChangeEdad.bind(this);
+    this.onChangeSexo = this.onChangeSexo.bind(this);
     this.saveTutorial = this.saveTutorial.bind(this);
     this.newTutorial = this.newTutorial.bind(this);
 
     this.state = {
       cedula: "",
-      nombre: "", 
-      edad: ""
+      nombre: "",
+      edad: "",
+      sexo: ""
     };
   }
 
@@ -35,11 +39,18 @@ export default class AddTutorial extends Component {
     });
   }
 
+  onChangeSexo(e) {
+    this.setState({
+      sexo: e.target.value
+    });
+  }
+
   saveTutorial() {
     var data = {
       cedula: this.state.cedula,
       nombre: this.state.nombre,
-      edad: this.state.edad
+      edad: this.state.edad,
+      sexo: this.state.sexo
     };
 
     TutorialDataService.create(data)
@@ -48,7 +59,8 @@ export default class AddTutorial extends Component {
           cedula: response.data.cedula,
           nombre: response.data.nombre,
           edad: response.data.edad,
-          
+          sexo: response.data.sexo,
+
 
           submitted: true
         });
@@ -64,7 +76,8 @@ export default class AddTutorial extends Component {
       cedula: "",
       nombre: "",
       edad: "",
-      
+      sexo: "",
+
 
       submitted: false
     });
@@ -72,61 +85,88 @@ export default class AddTutorial extends Component {
 
   render() {
     return (
-        <div className="submit-form">
-          {this.state.submitted ? (
-            <div>
-              <h4>You submitted successfully!</h4>
-              <button className="btn btn-success" onClick={this.newTutorial}>
-                Add
-              </button>
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            
+            <h4>Enviado Correctamente</h4>
+            <button className="btn btn-success" onClick={this.newTutorial}>
+              Crear Nueva Encuesta
+            </button>
+          </div>
+        ) : (
+          
+          <div>
+            <h3 class="text-hide">Encuesta De Caracterización</h3>
+            <div className="form-group">
+              <label htmlFor="cedula">Cedula</label>
+              <input
+                type="text"
+                className="form-control"
+                id="cedula"
+                required
+                value={this.state.cedula}
+                onChange={this.onChangeCedula}
+                name="cedula"
+              />
             </div>
-          ) : (
-            <div>
-              <div className="form-group">
-                <label htmlFor="cedula">Cedula</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="cedula"
-                  required
-                  value={this.state.cedula}
-                  onChange={this.onChangeCedula}
-                  name="cedula"
-                />
-              </div>
-  
-              <div className="form-group">
-                <label htmlFor="nombre">Nombre</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="nombre"
-                  required
-                  value={this.state.nombre}
-                  onChange={this.onChangeNombre}
-                  name="nombre"
-                />
-              </div>
 
-              <div className="form-group">
-                <label htmlFor="edad">Edad</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="edad"
-                  required
-                  value={this.state.edad}
-                  onChange={this.onChangeEdad}
-                  name="edad"
-                />
-              </div>
-  
-              <button onClick={this.saveTutorial} className="btn btn-success">
-                Submit
-              </button>
+            <div className="form-group">
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                className="form-control"
+                id="nombre"
+                required
+                value={this.state.nombre}
+                onChange={this.onChangeNombre}
+                name="nombre"
+              />
             </div>
-          )}
-        </div>
-      );
+
+            <div className="form-group">
+              <label htmlFor="edad">Edad</label>
+              <input
+                type="text"
+                className="form-control"
+                id="edad"
+                required
+                value={this.state.edad}
+                onChange={this.onChangeEdad}
+                name="edad"
+              />
+            </div>
+            <p>Sexo:</p>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value="Masculino"
+                  checked={this.state.sexo === "Masculino"}
+                  onChange={this.onChangeSexo}
+                />
+                Masculino
+              </label>
+            </div>
+            <div className="radio">
+              <label>
+                <input
+                  type="radio"
+                  value="Femenino"
+                  checked={this.state.sexo === "Femenino"}
+                  onChange={this.onChangeSexo}
+                />
+                Femenino
+              </label>
+
+            </div>
+
+            <button onClick={this.saveTutorial} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
   }
 }
